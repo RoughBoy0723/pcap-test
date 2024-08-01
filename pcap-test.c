647458de0d69eae7e7e525e77654697432c18c0c
RoughBoy <106831435+RoughBoy0723@users.noreply.github.com>
2024-08-01 22:29:29 +0900

    update_gilgil's FeedBack
    이경문 멘토님의 피드백을 추가하여 수정했습니다.
    1. IPHeader의 옵션에 의한 가변크기 고려
    2. ip,tcp의 가변 길이를 고려하여 데이터의 크기 설정
    3. eth_type를 이용하여 ip패킷만 필터
    4. 데이터의 사이즈가 20보다 언더일경우의 출력

--- pcap-test.c
+++ pcap-test.c
@@ -42,24 +42,36 @@ int main(int argc, char *argv[]){
 
         struct libnet_ethernet_hdr* eth_hdr = (struct libnet_ethernet_hdr *)packet;
         struct libnet_ipv4_hdr* ip_hdr = (struct libnet_ipv4_hdr *)(packet + LIBNET_ETH_H);
-        struct libnet_tcp_hdr* tcp_hdr = (struct libnet_tcp_hdr *)(packet + LIBNET_IPV4_H + LIBNET_ETH_H);
-        char pkt_data[20] = {' '};
+        struct libnet_tcp_hdr* tcp_hdr = (struct libnet_tcp_hdr *)(packet + (ip_hdr->ip_hl * 4) + LIBNET_ETH_H);
 
-        if(ip_hdr->ip_p == 0x06){
-            printf("Src Mac : %s\n", ether_ntoa((const struct ether_addr *)&eth_hdr->ether_shost));
-            printf("Dst Mac: %s\n", ether_ntoa((const struct ether_addr *)&eth_hdr->ether_dhost));
-            printf("Src Ip: %s\n", inet_ntoa(ip_hdr->ip_src));
-            printf("Dst Ip: %s\n", inet_ntoa(ip_hdr->ip_dst));
-            printf("Src Port: %d\n", ntohs(tcp_hdr->th_sport));
-            printf("Dst Port: %d\n", ntohs(tcp_hdr->th_dport));
+        uint16_t tcph_size = tcp_hdr->th_off * 4;
+        uint16_t iph_size = ip_hdr->ip_hl * 4;
+        printf("%d, %d, %d",htons(ip_hdr->ip_len),tcph_size, iph_size);
 
-            unsigned char * data = (unsigned char *)(packet + LIBNET_ETH_H  + LIBNET_IPV4_H + LIBNET_TCP_H);
-            printf("Data : ");
-            for(int i = 0; i< 20; i++){
-                printf("%02X ", data[i]);
-            }
-            printf("\n----------------------------------------\n");
+        if(eth_hdr->ether_type == 0x0800 && ip_hdr->ip_p == 0x06)
+            continue;
+        printf("Src Mac : %s\n", ether_ntoa((const struct ether_addr *)&eth_hdr->ether_shost));
+        printf("Dst Mac: %s\n", ether_ntoa((const struct ether_addr *)&eth_hdr->ether_dhost));
+        printf("Src Ip: %s\n", inet_ntoa(ip_hdr->ip_src));
+        printf("Dst Ip: %s\n", inet_ntoa(ip_hdr->ip_dst));
+        printf("Src Port: %d\n", ntohs(tcp_hdr->th_sport));
+        printf("Dst Port: %d\n", ntohs(tcp_hdr->th_dport));
+
+        unsigned char * data = (unsigned char *)(packet + LIBNET_ETH_H  + LIBNET_IPV4_H + LIBNET_TCP_H);
+
+        uint8_t data_size = htons(ip_hdr->ip_len) - tcph_size - iph_size;
+
+        printf("Data : ");
+
+        if( data_size < 20)
+            data_size = data_size;
+        else
+            data_size = 20;
+
+        for(int i = 0 ; i < data_size; i++){
+            printf("%02X ", data[i]);
         }
+        printf("\n----------------------------------------\n");
     }
     pcap_close(pcap);
 }
